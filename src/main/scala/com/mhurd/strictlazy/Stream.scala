464522e3dd223ef7d6947c99404ac57b7050aec6
mhurd <mjwhurd@gmail.com>
2014-05-28 13:03:29 +0100

    Started the exercises

---
+++ src/main/scala/com/mhurd/strictlazy/Stream.scala
@@ -0,0 +1,77 @@
+package com.mhurd.strictlazy
+
+import scala.annotation.tailrec
+
+sealed trait Stream[+A] {
+
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  // Exercise 1
+  def toList: List[A] = this match {
+    case Empty => List()
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  // Exercise 2a
+  def take(n: Int): Stream[A] = {
+    @tailrec
+    def inner(count: Int, s: Stream[A], result: List[A]): Stream[A] = {
+      if (count == n) Stream(result.reverse: _*) else {
+        s match {
+          case Empty => Stream(result.reverse: _*)
+          case Cons(h, t) => inner(count+1, t(), h() :: result)
+        }
+      }
+    }
+    inner(0, this, Nil)
+  }
+
+  // Exercise 2b
+  def drop(n: Int): Stream[A] = {
+    @tailrec
+    def inner(count: Int, result: Stream[A]): Stream[A] = {
+      if (count == 0) result else {
+        result match {
+          case Empty => result
+          case Cons(h, t) => inner(count-1, t())
+        }
+      }
+    }
+    inner(n, this)
+  }
+
+  // Exercise 3
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    @tailrec
+    def inner(count: Int, s: Stream[A], result: List[A]): Stream[A] = {
+      s match {
+        case Empty => Stream(result.reverse: _*)
+        case Cons(h, t) if p(h()) => inner(count+1, t(), h() :: result)
+        case _ => Stream(result.reverse: _*) // exit as predicate has failed to match
+      }
+    }
+    inner(0, this, Nil)
+  }
+
+}
+
+case object Empty extends Stream[Nothing]
+
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd // cache the head and tail to avoid repeated evaluation
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+}
---
+++ src/test/scala/com/mhurd/strictlazy/StreamSpec.scala
@@ -0,0 +1,46 @@
+package com.mhurd.strictlazy
+
+import org.scalatest._
+import com.mhurd.util.Timer
+
+class StreamSpec extends FlatSpec with Matchers with Timer {
+
+  "The Stream trait" should "have a toList function to convert to std Scala List" in {
+    val stream = Stream(1,2,3,4,5)
+    stream.toList.length should be(5)
+    stream.toList.map(_*2) should be (List(2,4,6,8,10))
+  }
+
+  it should "have a take(n) implementation" in {
+    val stream = Stream(1,2,3,4,5)
+    stream.take(0).toList should be (Nil)
+    stream.take(1).toList should be (List(1))
+    stream.take(2).toList should be (List(1, 2))
+    stream.take(3).toList should be (List(1, 2, 3))
+    stream.take(4).toList should be (List(1, 2, 3, 4))
+    stream.take(5).toList should be (List(1, 2, 3, 4, 5))
+    stream.take(6).toList should be (List(1, 2, 3, 4, 5))
+    stream.take(7).toList should be (List(1, 2, 3, 4, 5))
+  }
+
+  it should "have a drop(n) implementation" in {
+    val stream = Stream(1,2,3,4,5)
+    stream.drop(0).toList should be (List(1,2,3,4,5))
+    stream.drop(1).toList should be (List(2,3,4,5))
+    stream.drop(2).toList should be (List(3,4,5))
+    stream.drop(3).toList should be (List(4,5))
+    stream.drop(4).toList should be (List(5))
+    stream.drop(5).toList should be (Nil)
+    stream.drop(6).toList should be (Nil)
+    stream.drop(7).toList should be (Nil)
+  }
+
+  it should "have a takeWhile(A => Boolean) implementation" in {
+    val stream = Stream(1,2,3,4,5)
+    stream.takeWhile(_ < 3).toList should be (List(1,2))
+    stream.takeWhile(_ < 5).toList should be (List(1,2,3,4))
+    stream.takeWhile(_ % 2 != 0).toList should be (List(1))
+    stream.takeWhile(_ > 5).toList should be (Nil)
+  }
+
+ }
